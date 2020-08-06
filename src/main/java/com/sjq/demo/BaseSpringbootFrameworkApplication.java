dd5699962e26731bb0645ef9ed294b0fd0ad12be
yerxichen <879908188@qq.com>
2020-08-06 11:52:34 +0800

    修改不行运行的BUG

--- src/main/java/com/sjq/demo/BaseSpringbootFrameworkApplication.java
+++ src/main/java/com/sjq/demo/BaseSpringbootFrameworkApplication.java
@@ -1,14 +1,13 @@
 package com.sjq.demo;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
-import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
-@SpringBootApplication(exclude = {DruidDataSourceAutoConfigure.class, SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {DruidDataSourceAutoConfigure.class})
 @MapperScan("com.sjq.demo.mapper")
 public class BaseSpringbootFrameworkApplication {
 
